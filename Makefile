272981f0064bc69b18c9cf1418ba2c8888e1d06e
LongLifeHwang <Rhig0617@gmail.com>
2024-01-17 20:19:31 +0900

    Makefile

---
+++ Makefile
@@ -0,0 +1,27 @@
+CC = cc
+FLAG = -Wall -Wextra -Werror
+NAME = clean_room
+SOURCE = ./main.c \
+		./utils/ft_split.c 
+INC = ./clean_room.h
+OBJS = $(SOURCE:.c=.o)
+all : $(NAME)
+
+%.o : %.c $(INC)
+	$(CC) $(FLAG) -c $< -o $@ -I ./
+
+$(NAME): $(OBJS)
+	$(CC) $(OBJS) -o $(NAME)
+
+.PHONY : clean fclean re all
+clean : 
+	rm -f $(OBJS)
+
+fclean :
+	rm -f $(OBJS)
+	rm -f $(NAME)
+
+re :
+	rm -f $(OBJS)
+	rm -f $(NAME)
+	make all
--- main.c
+++ main.c
@@ -11,7 +11,7 @@ in : cooler(8)
 #include "clean_room.h"
 
 //ai yolo v5
-static void    child_process(int fd[2], char *argv[], char *envp[])
+static void    child_process(char *argv[], char *envp[])
 {
     char    **command;
 
@@ -21,30 +21,31 @@ static void    child_process(int fd[2], char *argv[], char *envp[])
     execve("/usr/bin/python3", command, envp);
 }
 
-static void     parents_process(int fd[2], char *argv[])
+static void     parents_process(void)
 {
     int     status;
 
     wait(&status);
     printf("%d\n",status);
     if (status == 0)
-        //code;
-    exit (0);
+        printf("code start");
 }
 
 int main(int argc, char *argv[], char *envp[])
 {
     pid_t   child;
-    int     fd[2];
 
     if (argc != 2)
         exit (0);
-    child = fork();  //python3 yolo
-    if (child < 0)
-        exit (1);
-    else if (child == 0) // child
-        child_process(fd, argv, envp);
-    else  //parents
-        parents_process(fd, argv);
+    while (1)
+    {
+        child = fork();  //python3 yolo
+        if (child < 0)
+            exit (1);
+        else if (child == 0) // child
+            child_process(argv, envp);
+        else  //parents
+            parents_process();
+    }
     exit (0);
 }
