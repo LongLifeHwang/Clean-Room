f17dbb39eb4218a87b0e285a93d45591d314ca0a
LongLifeHwang <Rhig0617@gmail.com>
2024-03-27 20:27:14 +0900

    헤더 교체

--- test/servo.c
+++ test/servo.c
@@ -1,12 +1,15 @@
-# include <wiringPi.h>
-# include <softPwm.h>
+#include <stdio.h>
+#include <wiringPi.h>
+#include <softPwm.h>
 
 # define InServo 18
 
 int main(void)
 {
-    wiringPiSetup();
-    softPwmCreate(InServo, 0, 200);
+    if(wiringPiSetup()==-1)
+        return 1;
+
+    softPwmCreate(SERVO, 0, 200);
     while (1)
     {
         softPwmWrite(InServo, 10);
