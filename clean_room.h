9dd7b03d78fa751fc324a50ddcff944f50b20ec3
LongLifeHwang <Rhig0617@gmail.com>
2024-03-28 19:25:12 +0900

    Wiringpi pin number setting

--- clean_room.h
+++ clean_room.h
@@ -37,9 +37,9 @@
 //https://blog.naver.com/simjk98/222149165719
 //입구 잠금 장치
 # define InServo 18
-# define InRemit 6 //22
+# define InRemit 22
 //출구 잠금 장치
-# define OutServo 18 //23
+# define OutServo 23
 # define OutRemit 26
 
 //https://m.blog.naver.com/simjk98/222133748955
