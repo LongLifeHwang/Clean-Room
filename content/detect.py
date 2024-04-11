ded682a1d03c43e4621feecdb8200815e10692da
LongLifeHwang <Rhig0617@gmail.com>
2024-04-11 14:33:31 +0900

    .

--- content/detect.py
+++ content/detect.py
@@ -265,6 +265,10 @@ def run(
             flag += 1
         # else:
         #     flag = 0
+        flag += 1
+        if (flag > 5):
+            cv2.destroyAllWindows()
+            sys.exit()
         # Print time (inference-only)
         # LOGGER.info(f"{s}{'' if len(det) else '(no detections), '}{dt[1].dt * 1E3:.1f}ms")
 
