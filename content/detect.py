5912e0b3a0a5dc46b6b830e5602c7a4a3a2f17a8
LongLifeHwang <Rhig0617@gmail.com>
2024-04-11 12:56:00 +0900

    detect.py

--- content/detect.py
+++ content/detect.py
@@ -266,9 +266,9 @@ def run(
         else:
             flag = 0
         if (flag >= 10):
-            exit(0)
+            print('O')
         # Print time (inference-only)
-        LOGGER.info(f"{s}{'' if len(det) else '(no detections), '}{dt[1].dt * 1E3:.1f}ms")
+        # LOGGER.info(f"{s}{'' if len(det) else '(no detections), '}{dt[1].dt * 1E3:.1f}ms")
 
     # Print results
     t = tuple(x.t / seen * 1e3 for x in dt)  # speeds per image
