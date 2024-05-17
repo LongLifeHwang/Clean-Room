88bdb2c71fd2792bb6d6ddf9525bcf1605878814
LongLifeHwang <Rhig0617@gmail.com>
2024-05-17 22:09:41 +0900

    .

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 git log
-git rebase -i 03b6c7d89bf6a6d7eee91ce760d04f159c4a3cb1
+git rebase -i 6055aba72376d7de9bb35b6dee3b134eb888ec24
 git commit --amend --no-edit --date="May 12 22:45:45 2024 +0000"
 git push -f
 
