600a462fe27d022bb15040b03e84f7def54fd5b0
OdOp <142278459+LongLifeHwang@users.noreply.github.com>
2024-05-30 11:45:48 +0900

    Update gui_thread.c

--- gui_thread.c
+++ gui_thread.c
@@ -45,21 +45,23 @@ static gboolean on_draw_event(GtkWidget *widget, cairo_t *cr, gpointer user_data
     return FALSE;
 }
 
-static gboolean time_handler(GtkWidget *widget)
+static gboolean time_handler(Data* data)
 {
-    if (data.motor1)
+    if (data->motor1)
     {
-        angle1 += 0.1;
-        if (angle1 > 2 * M_PI) angle1 = 0;
+        data->angle1 += 0.1;
+        if (data->angle1 > 2 * M_PI)
+            data->angle1 = 0;
     }
-    if (data.motor2)
+    if (data->motor2)
     {
-        angle2 += 0.1;
-        if (angle2 > 2 * M_PI) angle2 = 0;
+        data->angle2 += 0.1;
+        if (data->angle2 > 2 * M_PI)
+            data->angle2 = 0;
     }
-    if (data.motor1 || data.motor2)
-        gtk_widget_queue_draw(widget);
-    if (!data.status)
+    if (data->motor1 || data->motor2)
+        gtk_widget_queue_draw(data->widget);
+    if (!data->status)
         gtk_main_quit();
     usleep(100);
     return TRUE;
@@ -67,16 +69,16 @@ static gboolean time_handler(GtkWidget *widget)
 
 void	thread_function(void *temp)
 {
-    GtkWidget   *window;
-    GtkWidget   *darea;
+    Data        *data;
 
-    gtk_init(NULL, NULL);
-    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    darea = gtk_drawing_area_new();
-    gtk_container_add(GTK_CONTAINER(window), darea);
+    data = (Data*)temp;
+    // gtk_init(NULL, NULL);
+    // window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    // darea = gtk_drawing_area_new();
+    // gtk_container_add(GTK_CONTAINER(window), darea);
     g_signal_connect(G_OBJECT(darea), "draw", G_CALLBACK(on_draw_event), NULL);
     // g_signal_connect(window, "destroy", G_CALLBACK(on_window_destroy), NULL);
-    g_timeout_add(50, (GSourceFunc)time_handler, (gpointer)window);
+    g_timeout_add(50, (GSourceFunc)time_handler, data);
     gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
     gtk_window_set_default_size(GTK_WINDOW(window), 600, 300);
     gtk_window_set_title(GTK_WINDOW(window), "Motor Rotation Simulation");
